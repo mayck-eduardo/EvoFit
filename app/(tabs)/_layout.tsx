7a229b93a50f6e0000dfef3158b694d322285757
Mayck Eduardo <mayck_22.03@hotmail.com>
2025-11-07 17:28:31 -0300

    fix: correção erro página inicial

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,7 +20,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="workout" // TELA INICIAL
+        name="index" // TELA INICIAL
         options={{
           title: 'Treino do Dia', 
           tabBarIcon: ({ color, size }) => (
